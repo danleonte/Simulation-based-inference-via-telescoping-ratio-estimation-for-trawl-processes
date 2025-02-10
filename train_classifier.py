3be9da51c62cc7a028f59ee6297ade37c3ba1e5c
Dan Leonte <dl2916@ic.ac.uk>
2025-02-10 14:11:29 +0200

    fix accuracy bug
    fix accuracy bug

--- train_classifier.py
+++ train_classifier.py
@@ -266,7 +266,7 @@ def train_classifier(classifier_config_file_path):
             S = 2 * jnp.mean(pred_Y * Y)
             classifier_output = jax.nn.sigmoid(pred_Y)
             B = 2 * jnp.mean(classifier_output)
-            accuracy = jnp.mean(pred_Y == Y)
+            accuracy = jnp.mean(classifier_output == Y)
 
             return bce_loss, (S, B, accuracy, classifier_output)
 
