b3d590ea515bb5b2ea1b9f030c49dffc72e86105
Dan Leonte <leonted@login509-02-r.ibex.kaust.edu.sa>
2025-05-02 16:37:27 +0300

    a

--- calibrate.py
+++ calibrate.py
@@ -637,9 +637,9 @@ if __name__ == '__main__':
         # 'mu': ['04_12_04_41_11','04_12_12_59_45','04_12_00_32_46','04_11_20_26_03','04_12_08_53_27','04_12_08_53_57','04_12_05_42_50','04_12_12_21_06'],
         # 'sigma': ['04_12_04_28_49','04_12_00_26_44','04_12_12_37_42','04_12_05_36_55','04_12_12_37_35','04_12_11_18_04','04_12_08_35_55','04_12_09_33_30','04_12_05_59_51',                                '04_11_20_26_03']
         'acf': ['04_12_12_36_45'],
-        # 'beta': ['04_12_04_26_56'],
-        # 'mu': ['04_12_00_32_46'],
-        # 'sigma': ['04_12_04_28_49'],
+        'beta': ['04_12_04_26_56'],
+        'mu': ['04_12_00_32_46'],
+        'sigma': ['04_12_04_28_49'],
         # 'acf':['02_26_18_30_52', '02_28_16_37_11', '03_01_09_30_39', '03_01_21_17_21', '03_02_21_06_17','03_02_06_41_57'],
         # 'beta':['02_26_15_56_48', '02_26_16_02_10', '02_26_19_29_54', '02_26_19_37_09', '02_26_23_14_03', '02_27_02_50_03'],
         # 'mu':['03_03_16_41_47', '03_03_16_45_26', '03_03_18_35_58', '03_03_21_29_04', '03_04_01_33_54', '03_04_01_46_31'],
@@ -657,10 +657,10 @@ if __name__ == '__main__':
                     trained_classifier_path = os.path.join(
                         os.getcwd(), 'models', 'new_classifier', 'TRE_full_trawl', key, value)  # 'NRE_full_trawl '
 
-                # calibrate_new(trained_classifier_path, nr_batches, 1000)
-                calibrate_new(trained_classifier_path, nr_batches, 1500)
-                # calibrate_new(trained_classifier_path, nr_batches, 2000)
-                # calibrate_new(trained_classifier_path, nr_batches, 2500)
+                #calibrate_new(trained_classifier_path, nr_batches, 1000)
+                #calibrate_new(trained_classifier_path, nr_batches, 1500)
+                calibrate_new(trained_classifier_path, nr_batches, 2000)
+                #calibrate_new(trained_classifier_path, nr_batches, 2500)
                 # calibrate(trained_classifier_path, nr_batches, 3000)
                 # calibrate(trained_classifier_path, nr_batches, 3500)
 
